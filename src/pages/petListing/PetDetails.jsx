46d8f6261cb61741dd2d0f8bece6c6dc74282fc3
Mehebul02 <mehebul2123@gmail.com>
2024-06-02 01:14:44 +0600

    adtop added successfully

--- src/pages/petListing/PetDetails.jsx
+++ src/pages/petListing/PetDetails.jsx
@@ -12,6 +12,8 @@ import useAxiosCommon from "../../hooks/useAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "../shared/loadingSpinner/LoadingSpinner";
 import Skeleton from "../../components/skeleton/Skeleton";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 // import useAxiosCommon from "../../hooks/useAxiosCommon";
 // import { useQuery } from "@tanstack/react-query";
 // import { useLoaderData, useParams } from "react-router-dom";
@@ -21,32 +23,48 @@ const PetDetails = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [petDetails, setPetDetails] = useState({});
   const axiosCommon = useAxiosCommon();
+  const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
-//  tan stack query single data
+  //  tan stack query single data
   const { id } = useParams();
-  const { data: pet = {},isLoading } = useQuery({
+  const { data: pet = {}, isLoading } = useQuery({
     queryKey: ["pet", id],
     queryFn: async () => {
       const { data } = await axiosCommon.get(`/pet/${id}`);
       return data;
     },
   });
-  // if(isLoading){
-  //   return <LoadingSpinner/>
-  // }
+
   const openAdoptModal = (petId, petName, petImage) => {
-    setPetDetails({ petId, petName, petImage });
+    const time = Date.now();
+    const userName = {
+      name: user?.displayName,
+      email: user?.email,
+    };
+    setPetDetails({ petId, petName, petImage, time, userName });
     setIsModalOpen(true);
   };
 
   const closeAdoptModal = () => {
     setIsModalOpen(false);
   };
-
-  const handleSubmit = (event) => {
+  // handleSubmit
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Here, you would normally save the adoption request to the database
-    console.log("Adoption request submitted:", petDetails);
+
+    try {
+      const { data } = await axiosSecure.post("/adopts", petDetails);
+      if(data.insertedId){
+
+        toast.success('Adopt submit successfully')
+      }
+      // console.log(data);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message)
+
+    }
+
     setIsModalOpen(false);
   };
   return (
@@ -57,45 +75,45 @@ const PetDetails = () => {
         </Helmet>
 
         <div className="w-[700px] mx-auto ">
-          {isLoading?<Skeleton/>:
-<div className=" border rounded border-gray-300 p-5 m-5 ">
-            <img
-              src={pet.image}
-              alt="Pet"
-              className="w-[600px] h-[300px]  object-cover rounded mx-auto"
-            />
-            <div className="flex justify-between px-6 mt-3">
-              <div className="font-semibold font-poppins text-xl  mb-2">
-                {pet.name}
-                
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <div className=" border rounded border-gray-300 p-5 m-5 ">
+              <img
+                src={pet.image}
+                alt="Pet"
+                className="w-[600px] h-[300px]  object-cover rounded mx-auto"
+              />
+              <div className="flex justify-between px-6 mt-3">
+                <div className="font-semibold font-poppins text-xl  mb-2">
+                  {pet.name}
+                </div>
+                <p className="text-gray-700 font-poppins font-medium text-base flex items-center gap-2">
+                  {" "}
+                  <span>
+                    <MdDateRange />
+                  </span>
+                  {pet.age}
+                </p>
               </div>
-              <p className="text-gray-700 font-poppins font-medium text-base flex items-center gap-2">
-                {" "}
-                <span>
-                  <MdDateRange />
+
+              <p className="px-6 flex items-center gap-3 font-medium text-gray-700  font-poppins">
+                <span className="text-red-700">
+                  <CiLocationOn />{" "}
                 </span>
-                {pet.age}
-                
+                {pet.location}
               </p>
-            </div>
 
-            <p className="px-6 flex items-center gap-3 font-medium text-gray-700  font-poppins">
-              <span className="text-red-700">
-                <CiLocationOn />{" "}
-              </span>
-              {pet.location}
-              
-            </p>
-
-            <div className="text-center">
-              <button
-                className="mt-3 text-center px-4 py-2 bg-green-500 text-white rounded"
-                onClick={() => openAdoptModal("1", "Pet Name", "pet-image.jpg")}
-              >
-                Adopt
-              </button>
+              <div className="text-center">
+                <button
+                  className="mt-3 text-center px-4 py-2 bg-green-500 text-white rounded"
+                  onClick={() => openAdoptModal(pet._id, pet.name, pet.image)}
+                >
+                  Adopt
+                </button>
+              </div>
             </div>
-          </div>}
+          )}
 
           {isModalOpen && (
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
@@ -106,19 +124,11 @@ const PetDetails = () => {
                 >
                   <VscChromeClose className="text-3xl text-black" />{" "}
                 </button>
-                <h2 className="text-2xl mb-4">{petDetails.petName}</h2>
+                <h2 className="text-2xl font-poppins mb-4">{pet.name}</h2>
                 <form onSubmit={handleSubmit} className="space-y-4">
-                  <input type="hidden" name="petId" value={petDetails.petId} />
-                  <input
-                    type="hidden"
-                    name="petName"
-                    value={petDetails.petName}
-                  />
-                  <input
-                    type="hidden"
-                    name="petImage"
-                    value={petDetails.petImage}
-                  />
+                  <input type="hidden" name="petId" value={pet._id} />
+                  <input type="hidden" name="petName" value={pet.name} />
+                  <input type="hidden" name="petImage" value={pet.image} />
                   <div>
                     <label htmlFor="userName" className="block text-gray-700">
                       User Name
