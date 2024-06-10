6076cc69b4e8376f44cb4631b6b13d2ec0b765f7
Mehebul02 <mehebul2123@gmail.com>
2024-06-10 19:54:38 +0600

    readme added

--- src/hooks/useAxiosCommon.jsx
+++ src/hooks/useAxiosCommon.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosCommon = axios.create({
-  // baseURL: 'https://server-site-tawny.vercel.app',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://server-site-tawny.vercel.app',
+  // baseURL: 'http://localhost:5000',
 });
 const useAxiosCommon = () => {
   return axiosCommon;
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://server-site-tawny.vercel.app'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://server-site-tawny.vercel.app'
+    // baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
     const navigate = useNavigate();
